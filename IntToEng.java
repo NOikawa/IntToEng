d277bf80fc0c639fe643d3b3bde69cf3adbb271f
NOikawa <65391073+NOikawa@users.noreply.github.com>
2020-06-17 16:43:07 +0900

    10桁版を実装しようと思ったが時間内に終わらなかったもの　動かない

--- IntToEng.java
+++ IntToEng.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class IntToEng {
 	// メインメソッド
 	//　数字配列
-	static String[] number = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-	static String[] teen = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+	static String[] number = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+	static String[] teen = { "","eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
 			"nineteen" };
-	static String[] mult10 = { "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+	static String[] mult10 = {"", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+	static String[] keta = {"" ,"thousand","million", "billion"};
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -18,9 +19,46 @@ public class IntToEng {
 	static String translateEng(int n) {
 		String str = "";
 		int digit = String.valueOf(n).length();
+		int roop = (digit/3) + 1;
+		int amari = digit % 3;
+		//後ろから3桁ずつ切り分ける
+		String nToString = String.valueOf(n);
+		int[] ThreeketaArr = new int[roop];
+		int i = 0;
+		if(amari != 0) {
+			ThreeketaArr[0] = Integer.parseInt(nToString.substring(0,amari));
+			i++;
+		}
+		for(i=0;i<roop;i++)  ThreeketaArr[i] = Integer.parseInt(nToString.substring(i,3));
+		int j = roop;
+		for(int k=0;k<roop;j++) {
+			str = str + getHundred(ThreeketaArr[k],String.valueOf(ThreeketaArr[k]).length()) + keta[j];
+			j--;
+		}
+
+		return str;
+	}
+
+	// 10~19
+	static String translateTeen(int n) {
+		return teen[n - 10];
+	}
+
+	static String translateMult10(int n) {
+		if (n % 10 == 0)
+			return mult10[(n / 10)];
+		else
+			return mult10[(n / 10)] + " " + translateNumber(n % 10);
+	}
+
+	static String translateNumber(int n) {
+		return number[n];
+	}
+
+	static String getHundred(int n, int digit) {
+		String str = "";
+
 		switch (digit) {
-		case 4:
-			str = str + translateNumber((n / 1000) % 10) + " thousand ";
 		case 3:
 			str = str + translateNumber((n / 100) % 10) + " hundred ";
 		case 2:
@@ -36,23 +74,5 @@ public class IntToEng {
 			break;
 		}
 		return str;
-	}
-
-
-
-	// 10~19
-	static String translateTeen(int n) {
-		return teen[n - 11];
-	}
-
-	static String translateMult10(int n) {
-		if (n % 10 == 0)
-			return mult10[(n / 10) - 1];
-		else
-			return mult10[(n / 10) - 1] + " " + translateNumber(n % 10);
-	}
-
-	static String translateNumber(int n) {
-		return number[n - 1];
 	}
 }
